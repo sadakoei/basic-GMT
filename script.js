e1523d8e16cc4e89014cbf023376a8739db9f17c
Doki-Doki-Doki <ryle_andrei_cabudol@dlsl.edu.ph>
2024-03-14 15:00:19 +0800

    header 100%

---
+++ script.js
@@ -0,0 +1,8 @@
+const burgir = document.querySelector(".burgir__menu")
+const hNav = document.querySelector(".header__nav")
+burgir.addEventListener("click", () =>{
+    burgir.classList.toggle("open");
+    hNav.classList.toggle("open");
+})
+
+
